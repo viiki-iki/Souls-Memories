f198e01fcdfb445c55bb8cc4d594933ab80cd42b
Vitoria <vitoria.i.fiorin@gmail.com>
2024-07-08 13:31:19 -0300

    derrota

--- Assets/Player/Scripts/PlayerMovement.cs
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
 public class PlayerMovement : MonoBehaviour
 {
     private Vector2 destination;
@@ -27,31 +26,39 @@ public class PlayerMovement : MonoBehaviour
             Vector2 mousePosition = Mouse.current.position.ReadValue();
             Vector2 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
-            RaycastHit2D hitGround = Physics2D.Raycast(worldPosition, Vector2.zero, Mathf.Infinity, groundLayer);
-            if(hitGround.collider != null)
-            {
-                destination = hitGround.point;
+            // RaycastHit2D hitGround = Physics2D.Raycast(worldPosition, Vector2.zero, Mathf.Infinity, groundLayer);
+            // if(hitGround.collider != null)
+            //  {
+            //destination = hitGround.point;
+            destination = worldPosition;
                 isMoving = true;
-            }
+          //  }
             //print(mousePosition);
             //print(destination);
         }
     }
 
-   // private void AdjustPerspective()
-   // {
-   //     Vector3 scale = transform.localScale;
-   //     scale.x = perspectiveScale / (scaleRatio - transform.position.y);
-   //     scale.y = perspectiveScale / (scaleRatio - transform.position.y);
-   //     transform.localScale = scale;
-   // }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("SceneEdge"))
+        {
+            isMoving = false;
+        }
+    }
+
+    // private void AdjustPerspective()
+    // {
+    //     Vector3 scale = transform.localScale;
+    //     scale.x = perspectiveScale / (scaleRatio - transform.position.y);
+    //     scale.y = perspectiveScale / (scaleRatio - transform.position.y);
+    //     transform.localScale = scale;
+    // }
 
     void Update()
     {        
         if (isMoving)
-        {         
+        {
             transform.position = Vector2.MoveTowards(transform.position, destination, speed * Time.deltaTime);          
-
             if ((Vector2)transform.position == destination)         
             {               
                 isMoving = false;         
