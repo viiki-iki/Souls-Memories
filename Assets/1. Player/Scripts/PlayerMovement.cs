2e15d8e5108c1ea5f30075b710de0cd2d6bc161c
Vitoria <vitoria.i.fiorin@gmail.com>
2024-07-12 19:41:32 -0300

    update before tests

--- Assets/1. Player/Scripts/PlayerMovement.cs
+++ Assets/1. Player/Scripts/PlayerMovement.cs
@@ -7,7 +7,6 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 destination;
     private Vector2 detourPosition;
     private Rigidbody2D rb;
-    private PolygonCollider2D playerCollider;
 
     private bool isMoving;
     private bool isDetouring;
@@ -19,22 +18,18 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 detourPos1;
     private Vector2 detourPos2;
 
-
-    // [SerializeField] private float perspectiveScale;
-    // [SerializeField] private float scaleRatio;
-
     [SerializeField] private float speed;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private LayerMask obstacleLayer;
-    
+
+    // [SerializeField] private float perspectiveScale;
+    // [SerializeField] private float scaleRatio;
 
     private void Start()
     {
         isMoving = false;
         isDetouring = false;
         rb = GetComponent<Rigidbody2D>();
-        playerCollider = GetComponent<PolygonCollider2D>(); // Obtém o Collider do jogador
-
         // AdjustPerspective();
     }
 
@@ -45,10 +40,12 @@ public class PlayerMovement : MonoBehaviour
             Vector2 mousePosition = Mouse.current.position.ReadValue();
             Vector2 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);             
             RaycastHit2D hitGround = Physics2D.Raycast(worldPosition, Vector2.zero, Mathf.Infinity, groundLayer);
-            if (hitGround.collider != null) //futuramente add movimentaçao p/ o objeto interactable
+            RaycastHit2D hitObs = Physics2D.Raycast(worldPosition, Vector2.zero, Mathf.Infinity, obstacleLayer);
+
+            if (hitGround.collider != null && hitObs.collider == null) //futuramente add movimentaçao p/ o objeto interactable
             {
-                destination = hitGround.point;
-                // destination = worldPosition;
+                //closestpoint verificaçao (quando clicar em interactable ir no closestpoint e nao onde clicou)
+                destination = hitGround.point;// destination = worldPosition;
 
                 RaycastHit2D[] obstaclesHit = Physics2D.RaycastAll(transform.position, destination - (Vector2)transform.position, Vector2.Distance(transform.position, destination), obstacleLayer);
                 foreach (var item in obstaclesHit)
@@ -56,24 +53,17 @@ public class PlayerMovement : MonoBehaviour
                     if (item.collider != null)
                     {
                         Debug.Log("Obstacle in the way: ");
-
-                        //Collider                       
+                       
                         Collider2D obsCollider = item.collider;
                         Vector2 obsSize = obsCollider.bounds.size;
-                        float detourDistance = Mathf.Min(obsSize.x, obsSize.y) + 0.5f;
+                        //float detourDistance = Mathf.Min(obsSize.x, obsSize.y) + 0.5f;
+                        float detourDistance = Mathf.Max(0.5f, Mathf.Min(obsSize.x, obsSize.y)/ 2f); // + 0.5f;
 
                         //ponto de desvio
                         Vector2 direction = (destination - (Vector2)transform.position).normalized;
                         Vector2 perpDirection = Vector2.Perpendicular(direction);
-                        // Vector2 detourPos1 = item.point + perpDirection * detourDistance;
-                        // Vector2 detourPos2 = item.point - perpDirection * detourDistance;
-                        // bool detour1Clear = RaycastVerification(transform.position, detourPos1);
-                        // bool detour2Clear = RaycastVerification(transform.position, detourPos2);
-                        // bool end1Clear = RaycastVerification(detourPos1, destination);
-                        // bool end2Clear = RaycastVerification(detourPos2, destination);
 
-                        StartCoroutine(DetourOptionsCheck(item.point, perpDirection, detourDistance));
-                       
+                        StartCoroutine(PathOptionsCheck(item.point, perpDirection, detourDistance));                      
                         isDetouring = true;
                     }
                 }
@@ -84,7 +74,7 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    IEnumerator DetourOptionsCheck(Vector2 itemPoint, Vector2 perpDirection, float detourDistance)
+    IEnumerator PathOptionsCheck(Vector2 itemPoint, Vector2 perpDirection, float detourDistance)
     {
         //verificar os 2 lados do obs
         detourPos1 = itemPoint + perpDirection * detourDistance;
@@ -98,40 +88,21 @@ public class PlayerMovement : MonoBehaviour
         if (detour1Clear && detour2Clear)
         {
             if (end1Clear && end2Clear)
-            {
                 detourPosition = Vector2.Distance(transform.position, detourPos1) < Vector2.Distance(transform.position, detourPos2) ? detourPos1 : detourPos2;
-            }
             else if (end1Clear)
-            {
                 detourPosition = detourPos1;
-            }
             else if (end2Clear)
-            {
                 detourPosition = detourPos2;
-            }
         }
-        else if (detour1Clear)
+        else if (detour1Clear && end1Clear)
+            detourPosition = detourPos1;           
+        else if (detour2Clear && end2Clear)               
+            detourPosition = detourPos2;            
+        else    // quando os 2 lados n dao certo
         {
-            if (end1Clear)
-            {
-                detourPosition = detourPos1;
-            }
-        }
-        else if (detour2Clear)
-        {
-            if (end2Clear)
-            {
-                detourPosition = detourPos2;
-            }
-        }
-        else // quando os 2 lados n dao certo
-        {
-
             print("alterando caminho");
             isMoving = false;
-            DetourOptionsCheck(itemPoint, perpDirection, detourDistance + 0.5f);
-         
-           // return;
+            StartCoroutine(PathOptionsCheck(itemPoint, perpDirection, detourDistance + 0.5f));
         }
         yield break;
     }
